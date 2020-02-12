bf63f55f85805464542c06af82f6784adae0d00a
Phong Cao <thcao@microsoft.com>
2020-02-12 10:26:48 -0500

    Fixed compiling issues on AndroidX.

--- android/src/main/java/com/azure/reactnative/notificationhub/ReactNativeNotificationsHandler.java
+++ android/src/main/java/com/azure/reactnative/notificationhub/ReactNativeNotificationsHandler.java
@@ -17,11 +17,9 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.content.LocalBroadcastManager;
+import androidx.core.app.NotificationCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import android.util.Log;
-
-import com.facebook.react.bridge.ReadableMap;
 
 import org.json.JSONArray;
 import org.json.JSONException;
